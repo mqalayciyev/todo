7a99aeddd861df22a5e52bf5ef7703f40a387f65
mqalayciyev <mqalayciyev@mail.ru>
2021-11-15 22:05:10 +0400

    OWLCAROUSEL vs DROPDOWN

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,10 +5,117 @@ import { Link } from 'react-router-dom';
 class Header extends Component {
     render() {
         return (
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/cart">Shopping Cart</Link>
-            </div>
+            <>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+
+
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/cart">Shopping Cart</Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                    Dropdown
+                                </a>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a className="dropdown-item" href="#">Action</a>
+                                    <a className="dropdown-item" href="#">Another action</a>
+                                    <div className="dropdown-divider"></div>
+                                    <a className="dropdown-item" href="#">Something else here</a>
+                                    <div className="position-relative sub-menu-toggle">
+                                        <div className="dropdown-item" data-toggle="sub-menu">
+                                            Dropright
+                                            <i class="fa fa-chevron-right" style={{ verticalAlign: 'middle' }}></i>
+                                        </div>
+                                        <div className="sub-menu">
+                                            <a className="sub-menu-item" href="#">Link 1</a>
+                                            <a className="sub-menu-item" href="#">Link 2</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#loginModal">
+                            Login
+                        </button>
+                    </div>
+                </nav>
+
+
+
+
+                <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="loginModalLabel">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div className="form-group">
+                                    <input type="text" className="form-control" placeholder='Username' />
+                                </div>
+                                <div className="form-group">
+                                    <input type="password" className="form-control" placeholder='Password' />
+                                </div>
+                                <button className="btn btn-success">Login</button>
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#registerModal">
+                                    Register
+                                </button>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="registerModalLabel">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div className="form-group">
+                                    <input type="text" className="form-control" placeholder='Last Name' />
+                                </div>
+                                <div className="form-group">
+                                    <input type="text" className="form-control" placeholder='First Name' />
+                                </div>
+                                <div className="form-group">
+                                    <input type="text" className="form-control" placeholder='Email' />
+                                </div>
+                                <div className="form-group">
+                                    <input type="password" className="form-control" placeholder='Password' />
+                                </div>
+                                <button className="btn btn-success">Register</button>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,12 +7,21 @@ function Home(props) {
     return (
         <>
             <div className="container">
-                <div className="row justify-content-center">
+                {/* <div className="row justify-content-center">
                     {
                         data.map(item => {
                             return <Card item={item} />
                         })
                     }
+                </div> */}
+
+                <div class="owl-carousel">
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" />
+                <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+                <img src="https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg" />
+                <img src="https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270" />
+                <img src="https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg" />
+                <img src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__480.jpg" />
                 </div>
             </div>
 
